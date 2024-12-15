cd897b57b42807f2eebe050d04f459dc4e44866f
Karan <karanprince85@gmail.com>
2024-12-15 13:28:37 +0530

    sa

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def add(a,b):
+    print(a+b)
+
+a = int(input("Please enter one number - >"))
+b = int(input("Please enter another number - >")) 
+
+print("Addition of both the number is - >",add(a,b))
