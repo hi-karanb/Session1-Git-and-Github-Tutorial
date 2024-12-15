d218d704b7f572fd5aadc391353d23a8a3e2339a
Karan <karanprince85@gmail.com>
2024-12-15 13:41:28 +0530

    save

--- calculator.py
+++ calculator.py
@@ -4,8 +4,13 @@ def add(a,b):
 def sub(a,b):
     return a-b
 
+def mul(a,b):
+    return a*b
+
+
 a = int(input("Please enter one number - >"))
 b = int(input("Please enter another number - >")) 
 
 print("Addition of both the number is - >",add(a,b))
 print("Subtraction of both the number is - >",sub(a,b))
+print("Multiplication of both the number is - >",mul(a,b))
